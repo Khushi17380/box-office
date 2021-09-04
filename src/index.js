85490943596f04f398215457efb77113cf3532c3
Khushi <khushisahu850@gmail.com>
2021-09-04 13:18:06 +0530

    added react router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter} from 'react-router-dom';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render( <App /> ,
+ReactDOM.render( 
+<BrowserRouter>
+<App />
+</BrowserRouter>,
   document.getElementById('root')
 );
 
