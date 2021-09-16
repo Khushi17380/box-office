7f294ad4c36a31816e644b04e89cd7460c855081
Khushi <khushisahu850@gmail.com>
2021-09-16 23:59:07 +0530

    styled added animation to card using react fade in

--- src/components/Navs.js
+++ src/components/Navs.js
@@ -1,25 +1,32 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+import { NavList, LinkStyled } from './Navs.styled';
+
+const LINKS = [
+  { to: '/', text: 'Home' },
+  { to: '/starred', text: 'Starred' },
+];
 
 const Navs = () => {
-  const LINKS=[
-    {to:'/',text:'Home'},
-    {to:'/starred',text:'starred'}
-  ]
+  const location = useLocation();
+
   return (
     <div>
-      <ul>
-        {
-          LINKS.map(item=>(
+      <NavList>
+        {LINKS.map(item => (
           <li key={item.to}>
-            <Link to={item.to}>{item.text}</Link>
-            </li>
-            )
-        )}
-      </ul>
-      
+            <LinkStyled
+              to={item.to}
+              className={item.to === location.pathname ? 'active' : ''}
+            >
+              {item.text}
+            </LinkStyled>
+          </li>
+        ))}
+      </NavList>
     </div>
-  )
-}
+  );
+};
 
-export default Navs
+export default Navs;
